e8d95fe2da4687cefab29e8710ffb4827c3a008b
Jitka Novotná <jitka@ucw.cz>
2018-08-24 13:41:36 +0200

    simple build scripts

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+touch /var/lib/dspace/buildlock
+#git pull
+mvn clean
+mvn package -Dmirage2.on=true
+cp /opt/dspace.build/dspace.cfg /opt/dspace.build/dspace/target/dspace-installer/config/
+cd /opt/dspace.build/dspace/target/dspace-installer
+ant fresh_install
+cd /opt/dspace.build/
+sudo systemctl restart tomcat
+rm /var/lib/dspace/build
+rm -f /var/lib/dspace/buildlock
+
