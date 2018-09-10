021273020af8b50caadbcd237a6639f19ed06b57
Jitka Novotná <jitka@ucw.cz>
2018-09-10 12:04:06 +0200

    rebuild without restar

--- build.sh
+++ build.sh
@@ -8,6 +8,7 @@ cp /opt/dspace.build/dspace.cfg /opt/dspace.build/dspace/target/dspace-installer
 cd /opt/dspace.build/dspace/target/dspace-installer
 ant fresh_install
 cd /opt/dspace.build/
-sudo systemctl restart tomcat
+#Rebuild https://stackoverflow.com/questions/6583502/how-do-i-update-a-tomcat-webapp-without-restarting-the-entire-service
+#sudo systemctl restart tomcat
 rm -f /var/lib/dspace/buildlock
 
